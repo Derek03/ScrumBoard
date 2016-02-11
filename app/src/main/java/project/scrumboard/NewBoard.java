bbfd9596ec337cbf56294d962afe486b2984ebdd
Derek03 <derek.braden.larson@gmail.com>
2016-02-11 09:08:51 -0800

    Here goes the commit, hope it works.

---
+++ app/src/main/java/project/scrumboard/NewBoard.java
@@ -0,0 +1,13 @@
+package project.scrumboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_board);
+    }
+}
