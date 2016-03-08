3fd508f6b120a969c7666cb8fad166f77b8733b9
Francis Quintal <francisquintal.fq@gmail.com>
2016-03-07 16:28:02 -0800

    new board adding name of columns and rows to page

--- app/src/main/java/project/scrumboard/NewBoard.java
+++ app/src/main/java/project/scrumboard/NewBoard.java
@@ -1,40 +1,132 @@
 package project.scrumboard;
 
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class NewBoard extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private Button add;
+public class NewBoard extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView lst1;
+    private ListView lst2;
+    private ArrayList<String> columnValues = new ArrayList<>();
+    private ArrayList<String> rowValues = new ArrayList<>();
+    private ArrayAdapter<String> rowAdapter;
+    private ArrayAdapter<String> colAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_board);
 
-        add = (Button)findViewById(R.id.add);
-        add.setOnClickListener(Listener);
+        //Column Listview
+        lst1 = (ListView) findViewById(R.id.listCol);
+        colAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, columnValues);
+        lst1.setAdapter(colAdapter);
+        lst1.setOnItemClickListener(this);
+
+
+        //Row Listview
+        lst2 = (ListView) findViewById(R.id.listRow);
+        rowAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, rowValues);
+        lst2.setAdapter(rowAdapter);
+        lst2.setOnItemClickListener(this);
+
+
     }
 
-    View.OnClickListener Listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = null;
-            switch(view.getTag().toString()) {
-                case "Add":
-                    intent = new Intent(NewBoard.this, PostIt.class);
-                    break;
-                default:
-                    break;
-            }
 
-            startActivity(intent);
 
-            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            //       .setAction("Action", null).show();
-        }
-    };
+
+    //clicking list item
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        //do stuff here when clicked
+    }
+
+
+
+
+
+    //clicking the add column button
+    public void newCol(View view){
+
+        //edit box for alert dialog
+        final EditText textboxString = new EditText(this);
+
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage("Enter the name of your new column:")
+                .setView(textboxString)
+                .setTitle("Add column")
+                .setPositiveButton("Add",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = textboxString.getText().toString();
+                        columnValues.add(value);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        return;
+                    }
+                });
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+
+
+
+
+
+    //clicking the add column button
+    public void newRow(View view) {
+        final EditText textboxString = new EditText(this);
+
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage("Enter the name of your new Row:")
+                .setView(textboxString)
+                .setTitle("Add column")
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = textboxString.getText().toString();
+                        rowValues.add(value);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        return;
+                    }
+                });
+
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
 }
